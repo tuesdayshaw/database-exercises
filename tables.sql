f3053537281b82b68bcfdbf78ebbee8a67072809
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-05-31 10:30:22 -0500

    gitignore

--- tables.sql
+++ tables.sql
@@ -1,1 +1,26 @@
 -- Tables
+
+
+create table quotes (
+  author_first_name VARCHAR(50),
+  author_last_name VARCHAR(100) NOT NULL,
+  content TEXT NOT NULL
+);
+
+create table airports (
+  airport_code CHAR(3),
+  location VARCHAR(255),
+  description TEXT
+);
+
+create table people (
+  name VARCHAR(255) NOT NULL,
+  age DECIMAL(4, 1) NOT NULL,
+  salary DOUBLE
+);
+
+create table users (
+  username VARCHAR(255) NOT NULL ,
+  password VARCHAR(255) NOT NULL ,
+  is_admin TINYINT NOT NULL
+);
