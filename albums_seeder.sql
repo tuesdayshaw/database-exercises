edade0a09da8f20acf2b421e726405971a53742f
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-05-31 12:18:29 -0500

    completed lesson -insert table data together in single command

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,11 +1,12 @@
 
 USE codeup_test_db;
 
-INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('Michael Jackson', 'Thriller', 1982, 46.3, 'Rock, Pop, R&B');
-INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('AC/DC', 'Back in Black', 1980, 26.1, 'Hard Rock');
-INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('Pink Floyd', 'Dark Side of the Moon', 1973, 24.2, 'Prog Rock');
-INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'R&B, Soul, Pop, Soundtrack');
-INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('Meatloaf', 'Bat out of Hell', 1977, 21.5, 'Hard rock, Progressive rock');
-INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('Eagles', 'Greatest Hits', 1976, 32.2, 'Rock, Soft rock, Folk rock');
-INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('BeeGees', 'Saturday Night Fever', 1977, 32.2, 'Disco');
-INSERT INTO albums(artist, name, release_date, sales, genre) VALUES ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock');
+INSERT INTO albums(artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 46.3, 'Rock, Pop, R&B'),
+       ('AC/DC', 'Back in Black', 1980, 26.1, 'Hard Rock'),
+       ('Pink Floyd', 'Dark Side of the Moon', 1973, 24.2, 'Prog Rock'),
+       ('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'R&B, Soul, Pop, Soundtrack'),
+       ('Meatloaf', 'Bat out of Hell', 1977, 21.5, 'Hard rock, Progressive rock'),
+       ('Eagles', 'Greatest Hits', 1976, 32.2, 'Rock, Soft rock, Folk rock'),
+       ('BeeGees', 'Saturday Night Fever', 1977, 32.2, 'Disco'),
+       ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft rock');
