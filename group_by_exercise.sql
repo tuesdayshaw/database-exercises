4b5858181c9de8460bf7cb7451025bbbde645474
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-01 16:51:46 -0500

    Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -2,8 +2,9 @@ SELECT DISTINCT title
   FROM titles
 GROUP BY title ASC ;
 
-SELECT DISTINCT last_name
+SELECT DISTINCT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%'
       AND last_name LIKE '%E'
-GROUP BY last_name;
+GROUP BY last_name, first_name;
+
