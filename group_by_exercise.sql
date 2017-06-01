486188b3d895697cbeb00e1d82e1842dd9147cdb
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-01 16:48:20 -0500

    Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY. The results should be:
     Eldridge
     Erbe
     Erde
     Erie
     Etalle

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,3 +1,9 @@
 SELECT DISTINCT title
   FROM titles
 GROUP BY title ASC ;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'E%'
+      AND last_name LIKE '%E'
+GROUP BY last_name;
