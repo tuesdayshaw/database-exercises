3f06704c58536c35ab65176b6d8d274cdd5b1211
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-01 14:03:35 -0500

    corrected my select for finding employees between 1990 & 1999

--- where_exercises.sql
+++ where_exercises.sql
@@ -8,10 +8,10 @@ SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%';
 
-SELECT 'All employees who were hired after 1990:' AS '';
+SELECT 'All employees who were hired between 1990 and 1999:' AS '';
 SELECT emp_no, first_name, last_name
 FROM employees
-WHERE hire_date > '1990%';
+WHERE hire_date LIKE '199%';
 
 SELECT 'All employees who were born on Christmas:' AS '';
 SELECT emp_no, first_name, last_name
