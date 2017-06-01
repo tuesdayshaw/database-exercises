f5a4200b3e7b9e79248de2d8efa8cc459fffb99c
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-01 15:27:22 -0500

    updated fifth conditional to search names with q and NOT qu

--- where_exercises.sql
+++ where_exercises.sql
@@ -39,4 +39,5 @@ AND birth_date LIKE '%-12-25';
 SELECT 'All employees whose last name includes a q:' AS '';
 SELECT first_name, last_name
 FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
