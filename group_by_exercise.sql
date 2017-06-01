2359b5f99c2105c31c8ff329a7e9bf613c5f0129
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-01 16:54:14 -0500

    Find the unique last names with a 'q' but not 'qu'. You may use either DISTINCT or GROUP BY. Your results should be:
     Chleq
     Lindqvist
     Qiwen

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -8,3 +8,8 @@ WHERE last_name LIKE 'E%'
       AND last_name LIKE '%E'
 GROUP BY last_name, first_name;
 
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+      AND last_name NOT LIKE '%qu%';
+
