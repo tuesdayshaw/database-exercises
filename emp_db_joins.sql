7176fd9bb18460cdec2ce99d0413ef3b16432769
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-02 16:41:59 -0500

    Find the current titles of employees currently working in the Customer Service department.
    Find the current salary of all current managers.

--- emp_db_joins.sql
+++ emp_db_joins.sql
@@ -1,8 +1,23 @@
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', d.dept_name AS 'Department Name'
+SELECT title, count(*)
 FROM employees as e
   JOIN dept_manager as dm
     ON e.emp_no = dm.emp_no
   JOIN departments as d
     ON dm.dept_no = d.dept_no
-WHERE e.gender = 'F';
+  JOIN titles as t
+    ON e.emp_no = t.emp_no
+WHERE d.dept_no = 'd009';
+
+SELECT d.dept_name, concat(e.first_name, ' ', e.last_name) AS 'Name', s.salary
+FROM employees as e
+  JOIN dept_manager as dm
+    ON e.emp_no = dm.emp_no
+  JOIN departments as d
+    ON dm.dept_no = d.dept_no
+  JOIN titles as t
+    ON e.emp_no = t.emp_no
+  JOIN salaries as s
+    ON e.emp_no = s.emp_no
+WHERE e.emp_no = dm.emp_no
+AND s.to_date > curdate();
 
