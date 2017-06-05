930ce016566a34cec0f80f22354c3b290568f671
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-05 12:19:18 -0500

    refactoring exercises from joins

--- emp_db_joins.sql
+++ emp_db_joins.sql
@@ -19,19 +19,6 @@ and d.dept_name = 'Customer Service'
 GROUP BY t.title;
 
 
-SELECT d.dept_name, concat(e.first_name, ' ', e.last_name) AS 'Name', s.salary
-FROM employees as e
-  JOIN dept_manager as dm
-    ON e.emp_no = dm.emp_no
-  JOIN departments as d
-    ON dm.dept_no = d.dept_no
-  JOIN titles as t
-    ON e.emp_no = t.emp_no
-  JOIN salaries as s
-    ON e.emp_no = s.emp_no
-WHERE e.emp_no = dm.emp_no
-AND s.to_date > now();
-
 SELECT d.dept_name as 'Department', concat(e.first_name, ' ', e.last_name) AS 'Name', s.salary as 'Salary'
 FROM salaries AS s
 JOIN employees AS e on s.emp_no = e.emp_no
