a7ea37f7c08e451319a4406c526421420895f742
Tuesday Shaw <tuesday.shaw@gmail.com>
2017-06-13 09:40:43 -0500

    moved drop table ads to same migration as users since they have a foreign key dependency

--- adlister_db/ads_migration.sql
+++ adlister_db/ads_migration.sql
@@ -1,10 +1,9 @@
 use adlister_db;
 
-DROP TABLE IF EXISTS ads;
 
 CREATE TABLE ads (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  user_id INT UNSIGNED DEFAULT NULL,
+  user_id INT UNSIGNED NOT NULL,
   title VARCHAR(255),
   description TEXT,
   FOREIGN KEY (user_id) REFERENCES users (id)
--- adlister_db/ads_seeder.sql
+++ adlister_db/ads_seeder.sql
@@ -4,7 +4,7 @@ TRUNCATE ads;
 
 INSERT INTO ads (user_id, title, description)
 VALUES (1, 'Shoes', 'adidas for sale'),
-  (2, 'TV', 'tv for sale'),
-  (3, 'Iphone', 'Iphone for sale'),
-  (4, 'Couch', 'couch for sale');
+       (2, 'TV', 'tv for sale'),
+       (3, 'Iphone', 'Iphone for sale'),
+       (4, 'Couch', 'couch for sale');
 
